ecfd5d98ee09eb52856a417150a6372cd840cbe3
Hamzeh <amir.hamzeh.eidi@gmail.com>
2026-08-23 09:32:34 +0330

    feat: add base template and static assets

--- multi_shop/settings.py
+++ multi_shop/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'multi_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
